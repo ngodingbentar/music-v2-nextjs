bccfd3e6a74648e817776a8f0983c9bcf5fdaa1c
ngodingbentar <dewaqintoro@gmail.com>
2022-11-09 22:51:18 +0700

    updated: _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,36 @@
-import Sidebar from '../components/Sidebar'
+import { Searchbar, TopPlay, Discover, MusicPlayer, Sidebar } from '../components'
+
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
-
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  console.log('store', store.getState())
+  const { activeSong } = store.getState().player;
+
   return (
     <Provider store={store}>
       <div>
         <div className="relative flex">
           <Sidebar/>
-          {/* <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]"> */}
           <div className="flex-1 flex flex-col">
-          <Component {...pageProps} />
+            <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+              <Searchbar />
+
+              <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+                <div className="flex-1 h-fit pb-40">
+                  <Component {...pageProps} />
+                </div>
+                <div className="xl:sticky relative top-0 h-fit">
+                  <TopPlay />
+                </div>
+              </div>
+            </div>
+            {activeSong?.title && (
+              <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
+                <MusicPlayer />
+              </div>
+            )}
           </div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Searchbar, TopPlay, Discover, MusicPlayer } from '../components'
-import { useSelector } from 'react-redux';
-
-// import styles from '../styles/Home.module.css'
-
+import { Discover } from '../components'
 export default function Home() {
-  const { activeSong } = useSelector((state) => state.player);
 
   return (
     <>
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
-        <Searchbar />
-
-        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
-          <div className="flex-1 h-fit pb-40">
-            <Discover />
-          </div>
-          <div className="xl:sticky relative top-0 h-fit">
-            <TopPlay />
-          </div>
-        </div>
-      </div>
-      {activeSong?.title && (
-        <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
-          <MusicPlayer />
-        </div>
-      )}
+      <Discover />
     </>
   )
 }
