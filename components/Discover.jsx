3f48ec56d2343c9172a9177821863cbfacdaff85
ngodingbentar <dewaqintoro@gmail.com>
2023-10-31 21:58:17 +0700

    updated: layout n ID

--- components/Discover.jsx
+++ components/Discover.jsx
@@ -6,6 +6,7 @@ import Loader from './Loader';
 import { useGetSongsByGenreQuery } from '../redux/services/shazamCore';
 import { selectGenreListId } from '../redux/features/playerSlice';
 import SongCard from './SongCard';
+import TopPlay from './TopPlay';
 
 function Discover() {
   const dispatch = useDispatch();
@@ -21,6 +22,9 @@ function Discover() {
 
   return (
     <div className="flex flex-col">
+      <div className="xl:sticky relative top-0 h-fit">
+        <TopPlay />
+      </div>
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
         <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
 
--- redux/services/shazamCore.js
+++ redux/services/shazamCore.js
@@ -12,7 +12,8 @@ export const shazamCoreApi = createApi({
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/world' }),
     getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
-    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+    // getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=ID` }),
     getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
     getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
     getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
